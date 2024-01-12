9bdcdf134239499b34ab01720305240162fcbde4
A-dith-ya <awedithya@gmail.com>
2024-01-12 10:18:22 -0800

    Add RestController annotation to TransectExplorerApplication

--- api/src/main/java/com/example/transectexplorer/TransectExplorerApplication.java
+++ api/src/main/java/com/example/transectexplorer/TransectExplorerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TransectExplorerApplication {
 
 	public static void main(String[] args) {
