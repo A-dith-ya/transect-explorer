5e46e6802335c17558485ff9334248489a47db7a
A-dith-ya <awedithya@gmail.com>
2024-01-14 23:49:30 -0800

    Add Coordinate entity

---
+++ api/src/main/java/com/example/transectexplorer/model/Coordinate.java
@@ -0,0 +1,29 @@
+package com.example.transectexplorer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class Coordinate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Transect transect;
+
+    @Column(nullable = false, length = 50)
+    private String coordinates;
+
+    protected Coordinate() {
+    };
+
+    public Coordinate(Transect transect, String coordinates) {
+        this.transect = transect;
+        this.coordinates = coordinates;
+    }
+}
