7b221666788eeea233c94d74a554cffb782511ce
A-dith-ya <awedithya@gmail.com>
2024-01-15 23:44:47 -0800

    Add CoordinateRepository for Coordinate entity

---
+++ api/src/main/java/com/example/transectexplorer/repository/CoordinateRepository.java
@@ -0,0 +1,12 @@
+package com.example.transectexplorer.repository;
+
+import java.util.List;
+
+import com.example.transectexplorer.model.Coordinate;
+import com.example.transectexplorer.model.Transect;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoordinateRepository extends CrudRepository<Coordinate, Long> {
+    List<Coordinate> findByTransect(Transect transect);
+}
