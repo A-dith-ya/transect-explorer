6ed39811c0f8ee29dd620e495da00322c53d56fd
A-dith-ya <awedithya@gmail.com>
2024-01-16 17:50:51 -0800

    Add UserController for User Management

---
+++ api/src/main/java/com/example/transectexplorer/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.transectexplorer.controller;
+
+import com.example.transectexplorer.model.User;
+import com.example.transectexplorer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        if (userRepository.existsById(id)) {
+            updatedUser.setId(id);
+            return userRepository.save(updatedUser);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
