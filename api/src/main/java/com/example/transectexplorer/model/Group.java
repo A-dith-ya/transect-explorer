79424eff4eaf3956434998490eda279a71aac1ac
A-dith-ya <awedithya@gmail.com>
2024-01-18 23:02:23 -0800

    Add get methods for Group entity attributes id and group name

--- api/src/main/java/com/example/transectexplorer/model/Group.java
+++ api/src/main/java/com/example/transectexplorer/model/Group.java
@@ -34,4 +34,12 @@ public class Group {
   public void setId(Long id) {
     this.id = id;
   }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getGroupName() {
+    return groupName;
+  }
 }
