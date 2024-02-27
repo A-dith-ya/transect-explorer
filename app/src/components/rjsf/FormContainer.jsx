b811c5ddac25ca7f0e0fc1d39b3424bf2a5feb93
A-dith-ya <awedithya@gmail.com>
2024-02-27 11:45:21 -0800

    Add FormContainer for reusability of form and submission

---
+++ app/src/components/rjsf/FormContainer.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+import ObjectFieldTemplate from "./template/ObjectFieldTemplate";
+import ArrayFieldTemplate from "./template/ArrayFieldTemplate";
+import SubmitButton from "./template/SubmitButton";
+import "./FormContainer.css";
+
+const FormContainer = ({ schema, uiSchema, onSubmitAction }) => {
+  const [formData, setFormData] = useState(null);
+
+  const handleSubmit = async ({ formData }) => {
+    onSubmitAction(formData);
+  };
+
+  return (
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      formData={formData}
+      onChange={({ formData }) => setFormData(formData)}
+      validator={validator}
+      templates={{
+        ObjectFieldTemplate,
+        ArrayFieldTemplate,
+        ButtonTemplates: { SubmitButton },
+      }}
+      onSubmit={handleSubmit}
+    />
+  );
+};
+
+export default FormContainer;
