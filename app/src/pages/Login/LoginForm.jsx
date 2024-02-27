110026bfccc5c680167381f30ab0f37e1f283a8a
A-dith-ya <awedithya@gmail.com>
2024-02-27 14:14:24 -0800

    Refactor Form pages to use FormContainer

--- app/src/pages/Login/LoginForm.jsx
+++ app/src/pages/Login/LoginForm.jsx
@@ -1,35 +1,15 @@
 import React from "react";
-import Form from "@rjsf/core";
-import validator from "@rjsf/validator-ajv8";
-import schema from "../../jsonSchemas/loginschema.json";
-import ObjectFieldTemplate from "../../components/rjsf/ObjectFieldTemplate";
-import SubmitButton from "../../components/rjsf/SubmitButton";
+import FormContainer from "../../components/rjsf/FormContainer";
+import { loginFormSchema } from "../../components/rjsf/schema/LoginFormSchema";
+import UISchemas from "../../components/rjsf/UISchema/UISchema";
 import { loginUser } from "../../services/UserService";
-import "./styles.css";
 
 const LoginForm = () => {
-  const [formData, setFormData] = React.useState(null);
-
-  const handleSubmit = async ({ formData }) => {
-    console.log(formData);
-    loginUser(formData);
-  };
-
-  const uiSchema = {
-    password: {
-      "ui:widget": "password",
-    },
-  };
-
   return (
-    <Form
-      schema={schema}
-      uiSchema={uiSchema}
-      formData={formData}
-      onChange={(e) => setFormData(e.formData)}
-      validator={validator}
-      templates={{ ObjectFieldTemplate, ButtonTemplates: { SubmitButton } }}
-      onSubmit={handleSubmit}
+    <FormContainer
+      schema={loginFormSchema}
+      uiSchema={UISchemas.loginUISchema}
+      onSubmitAction={loginUser}
     />
   );
 };
