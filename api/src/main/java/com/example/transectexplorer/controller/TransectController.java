43b78dddd12003f5b5c36b139a87994b6044ac4b
A-dith-ya <awedithya@gmail.com>
2024-03-05 23:14:23 -0800

    Implement endpoint to create transect

--- api/src/main/java/com/example/transectexplorer/controller/TransectController.java
+++ api/src/main/java/com/example/transectexplorer/controller/TransectController.java
@@ -1,13 +1,18 @@
 package com.example.transectexplorer.controller;
 
 import com.example.transectexplorer.model.Transect;
+import com.example.transectexplorer.model.User;
 import com.example.transectexplorer.repository.TransectRepository;
+import com.example.transectexplorer.repository.UserRepository;
+import com.example.transectexplorer.dto.TransectDTO;
 import com.example.transectexplorer.model.Group;
 import com.example.transectexplorer.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/transects")
@@ -18,6 +23,26 @@ public class TransectController {
 
     @Autowired
     private GroupRepository groupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<TransectDTO> createTransect(@RequestBody TransectDTO transectDTO) {
+        Optional<Group> group = groupRepository.findById(transectDTO.getGroupId());
+        Optional<User> user = userRepository.findById(transectDTO.getUserCreatorId());
+
+        if (group.isPresent() && user.isPresent()) {
+            Transect transect = new Transect(group.get(), user.get(), transectDTO.getTransectName(),
+                    transectDTO.getDescription(), transectDTO.getLocation(), transectDTO.getCoordinate());
+
+            transectRepository.save(transect);
+
+            return new ResponseEntity<>(transectDTO, HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
     @DeleteMapping("/{id}")
     public void deleteTransect(@PathVariable Long id) {
