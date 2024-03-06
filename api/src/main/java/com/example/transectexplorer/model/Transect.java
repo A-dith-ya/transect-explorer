5b7a8a283731218628f0826e8247f77e02292efd
A-dith-ya <awedithya@gmail.com>
2024-03-06 10:19:06 -0800

    Implement getters and setters for Transect entity

--- api/src/main/java/com/example/transectexplorer/model/Transect.java
+++ api/src/main/java/com/example/transectexplorer/model/Transect.java
@@ -46,4 +46,60 @@ public class Transect extends BaseEntity {
         this.location = location;
         this.coordinate = coordinate;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public User getUserCreator() {
+        return userCreator;
+    }
+
+    public void setUserCreator(User userCreator) {
+        this.userCreator = userCreator;
+    }
+
+    public String getTransectName() {
+        return transectName;
+    }
+
+    public void setTransectName(String transectName) {
+        this.transectName = transectName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(String coordinate) {
+        this.coordinate = coordinate;
+    }
 }
