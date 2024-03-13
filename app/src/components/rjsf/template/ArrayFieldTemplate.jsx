a6f8592c93023e15f2d6f3bdfd4ed87ad70addd6
selgert <elgerts17@mytru.ca>
2024-03-13 13:23:18 -0700

    updated array item field

--- app/src/components/rjsf/template/ArrayFieldTemplate.jsx
+++ app/src/components/rjsf/template/ArrayFieldTemplate.jsx
@@ -4,14 +4,40 @@ import addIcon from "../../../assets/add-item-icon.png";
 
 
 const ArrayFieldTemplate = (props) => {
+
+  const transformItemTitle = (arrayTitle, title) => {
+    // Remove the "s" at the end of the arrayTitle if it exists
+    arrayTitle = arrayTitle.endsWith("s") ? arrayTitle.slice(0, -1) : arrayTitle;
+    // Split the title by '-'
+    const parts = title.split('-');
+    // Convert the second part to an integer
+    const index = parseInt(parts[1]);
+    // Return the transformed title
+    return `${arrayTitle} ${index}`;
+  };
+
+  const updatedProps = {
+    ...props,
+    items: props.items.map((element, index) => {
+      return {
+        ...element,
+        children: [
+          React.cloneElement(element.children, {key: index,  title: transformItemTitle(props.title, element.children.props.title) })
+        ]
+      };
+    })
+  };
+
+
   return (
     <div className="arrayfield">
         <div className="arrayfield__title">
             <h3>{props.title}</h3>
             {props.canAdd && <button className="arrayfield__add-button" onClick={props.onAddClick}>Add</button>}
         </div>
-      {props.items.map((element) => (
+      {updatedProps.items.map((element) => (
         <div className="arrayfield__item" key={element.key}>
+          {console.log(updatedProps)}
           {element.children}
           {element.hasRemove && (
             <button className="arrayfield__remove-button" onClick={element.onDropIndexClick(element.index)}>
