4938a3384cc66258ec46f2efacf55a6a113b44b9
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-03-13 19:32:48 -0700

    className consistent
    Use className instead of class, to keep code consistent

--- app/src/pages/Error/NotFound.jsx
+++ app/src/pages/Error/NotFound.jsx
@@ -5,12 +5,12 @@ function NotFound() {
   const navigate = useNavigate();
   return (
     <div className="div_notfound">
-      <i class="fas fa-sad-tear fa-10x"></i>
+      <i className="fas fa-sad-tear fa-10x"></i>
       <div className="div_notfound">
         <h1>404 Page Not Found</h1>
         <p>Sorry, the page you are looking for is not looking for you!</p>
         <a onClick={() => navigate("/map")}>
-          <i class="fas fa-home"></i> Take me home... the place were I belong...
+          <i className="fas fa-home"></i> Take me home... the place were I belong...
         </a>
       </div>
     </div>
