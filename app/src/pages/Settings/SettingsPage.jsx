ef7c2015ee4f375f128df4629eae5d15014f071f
bburciaga <burciagab16@gmail.com>
2024-03-14 10:31:05 -0700

    updated settings page

--- app/src/pages/Settings/SettingsPage.jsx
+++ app/src/pages/Settings/SettingsPage.jsx
@@ -39,8 +39,52 @@ const SettingsPage = () => {
         </button>
       </div>
       <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="settings" style={{display: 'flex', flexFlow: 'column wrap', alignItems: 'start'}}>
+        <h3> Account Settings </h3>
+        <form>
+          {userData ? (
+            <p> {userData.username} </p>
+          ) : (
+            <ul>
+              <li>
+                {" "}
+                <h5>Username:</h5> <input />{" "}
+              </li>
+              <li>
+                {" "}
+                <h5>Email:</h5> <input />{" "}
+              </li>
+              <li>
+                {" "}
+                <h5>Password:</h5> <input />
+              </li>
+            </ul>
+          )}
+          <div className="edit" style={{display: 'flex', flexFlow: 'row nowrap'}}>
+            <button className="text-btn"> Save </button>
+
+            <button className="text-btn"> Cancel </button>
+          </div>
+        </form>
+        <div id="myModal" className="modal">
+          {" "}
+        </div>
       </div>
+      <dialog ref={dialogRef}>
+        <form method="dialog">
+          Are you sure you want to deactivate your account?
+          <div className="dialog">
+            <button>Yes</button>
+            <button
+              onClick={function () {
+                dialogRef.current.close();
+              }}
+            >
+              No
+            </button>
+          </div>
+        </form>
+      </dialog></div>
     </div>
   );
 };
