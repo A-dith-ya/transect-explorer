8bd6c9e6c5e3200ec31dc9fcc039e98faf0d4eca
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-03-29 20:31:31 +0000

    Transect List call API GetByUserCreator

--- app/src/pages/Region/TransectList.jsx
+++ app/src/pages/Region/TransectList.jsx
@@ -1,39 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { getTransectsByUserCreatorId } from "../../services/TransectService";
 import "./index.css";
-
-// Mock function to simulate fetching transects for a specific group
-// Replace this with your actual API call
-const fetchTransectsForGroup = async (groupId) => {
-  // Example transects fetched from the backend for a specific group
-  const transects = [
-    {
-      id: 1,
-      transectName: "Transect A",
-      description: "Description A",
-      location: "Region 1",
-      coordinate: "Coord A",
-    },
-    {
-      id: 2,
-      transectName: "Transect B",
-      description: "Description B",
-      location: "Region 1",
-      coordinate: "Coord B",
-    },
-    {
-      id: 3,
-      transectName: "Transect C",
-      description: "Description C",
-      location: "Region 2",
-      coordinate: "Coord C",
-    },
-    // Add more transects as needed
-  ];
-
-  // Simulating fetching transects related to the selected group
-  return transects;
-};
 
 const TransectList = ({ selectedGroupId }) => {
   const [transects, setTransects] = useState([]);
@@ -42,12 +10,18 @@ const TransectList = ({ selectedGroupId }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchAndSetTransects = async () => {
-      const fetchedTransects = await fetchTransectsForGroup(selectedGroupId);
-      setTransects(fetchedTransects);
+    const fetchTransects = async () => {
+      try {
+        const fetchedTransects = await getTransectsByUserCreatorId();
+        if (fetchedTransects) {
+          setTransects(fetchedTransects);
+        }
+      } catch (error) {
+        console.error("Error fetching transects:", error);
+      }
     };
 
-    fetchAndSetTransects();
+    fetchTransects();
   }, [selectedGroupId]);
 
   const sortTransects = (columnName) => {
@@ -73,45 +47,45 @@ const TransectList = ({ selectedGroupId }) => {
     <>
       <h1>Transect List</h1>
       <div className="table-div">
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  Transect Name
-                  <button
-                    className="button_arrows"
-                    onClick={() => sortTransects("transectName")}
-                  >
-                    <i className="fa-solid fa-arrows-alt-v"></i>
-                  </button>
-                </th>
-                <th>Description</th>
-                <th onClick={() => sortTransects("location")}>
-                  Location
-                  <button
-                    className="button_arrows"
-                    onClick={() => sortTransects("transectName")}
-                  >
-                    <i className="fa-solid fa-arrows-alt-v"></i>
-                  </button>
-                </th>
-                <th>Coordinate</th>
-              </tr>
-            </thead>
-            <tbody>
-              {transects.map((transect) => (
-                <tr
-                  key={transect.id}
-                  onClick={() => navigate("/region/transect")}
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Transect Name
+                <button
+                  className="button_arrows"
+                  onClick={() => sortTransects("transectName")}
                 >
-                  <td>{transect.transectName}</td>
-                  <td>{transect.description}</td>
-                  <td>{transect.location}</td>
-                  <td>{transect.coordinate}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                  <i className="fa-solid fa-arrows-alt-v"></i>
+                </button>
+              </th>
+              <th>Description</th>
+              <th onClick={() => sortTransects("location")}>
+                Location
+                <button
+                  className="button_arrows"
+                  onClick={() => sortTransects("transectName")}
+                >
+                  <i className="fa-solid fa-arrows-alt-v"></i>
+                </button>
+              </th>
+              <th>Coordinate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transects.map((transect) => (
+              <tr
+                key={transect.id}
+                onClick={() => navigate("/region/transect")}
+              >
+                <td>{transect.transectName}</td>
+                <td>{transect.description}</td>
+                <td>{transect.location}</td>
+                <td>{transect.coordinate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
--- app/src/services/TransectService.js
+++ app/src/services/TransectService.js
@@ -36,4 +36,20 @@ const getTransectID = async (id) => {
   }
 };
 
-export { createTransect, getTransectID };
+const getTransectsByUserCreatorId = async () => {
+  try {
+    const userCreatorId = sessionStorage.getItem("id");
+    if (userCreatorId) {
+      console.log("Session Storage: " + userCreatorId);
+      const result = await axios.get(`${baseURL}/users/${userCreatorId}`);
+      return result.data;
+    } else {
+      throw new Error("No User Id in session storage");
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error("Error getting transects by creator ID: " + error.message);
+  }
+};
+
+export { createTransect, getTransectID, getTransectsByUserCreatorId };
