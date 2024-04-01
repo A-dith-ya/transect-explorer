f5d897926d40cd79efd5ad5a069a22281cabbd08
selgert <elgerts17@mytru.ca>
2024-03-31 17:18:09 -0700

    updated settings and title ui

--- app/src/pages/Settings/SettingsPage.jsx
+++ app/src/pages/Settings/SettingsPage.jsx
@@ -6,8 +6,10 @@ import { useRef } from "react";
 
 const SettingsPage = () => {
   const [userData, setUserData] = useState(null);
-  const dialogRef = useRef();
+  const [data, setData] = useState([]);
   const [selected, setSelected] = useState(null);
+  const id = sessionStorage.getItem("id");
+  console.log(userData);
 
   const toggle = (index) => {
     if (selected == index) {
@@ -20,41 +22,58 @@ const SettingsPage = () => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const result = await getUser();
+        const result = await getUser(id);
         setUserData(result);
       } catch (error) {
         console.log(error);
       }
     };
     fetchUserData();
-  }, []);
+  }, [id]);
 
-  const data = [
-    {
-      title: "Username",
-      value: "TestUser",
-    },
-    {
-      title: "Email",
-      value: "UserEmailAddress",
-    },
-    {
-      title: "Password",
-      value: "Change password",
-    },
-    {
-      title: "Logout",
-      value: "Are you sure you want to logout?",
-    },
-  ];
+  useEffect(() => {
+    if (userData && userData.username) {
+      const data = [
+        {
+          title: "Username",
+          value: userData.username,
+          buttonName: "Username",
+          buttonIcon: "fa-solid fa-pen-to-square",
+          buttonType: "edit-button",
+        },
+        {
+          title: "Email",
+          value: userData.userEmail,
+          buttonName: "Email",
+          buttonIcon: "fa-solid fa-pen-to-square",
+          buttonType: "edit-button",
+        },
+        {
+          title: "Password",
+          value: "Would you like to change your password?",
+          buttonName: " Password",
+          buttonIcon: "fa-solid fa-pen-to-square",
+          buttonType: "edit-button",
+        },
+        {
+          title: "Logout",
+          value: "Are you sure you want to logout?",
+          buttonName: "Logout",
+          buttonIcon: "fa-solid fa-arrow-right-from-bracket",
+          buttonType: "delete-button",
+        },
+      ];
+      setData(data);
+    }
+  }, [userData]);
 
   return (
     <div className="settings-page">
-      <div className="settings-page-title">Account Settings</div>
+      <div className="page-title">Account Settings</div>
       <div className="wrapper">
         <div className="accordion">
           {data.map((item, index) => (
-            <div className="accordion-item">
+            <div className="accordion-item" key={`${item.title}-${index}`}>
               <div
                 className="accordion-item-title"
                 onClick={() => toggle(index)}
@@ -73,8 +92,11 @@ const SettingsPage = () => {
                     : "accordion-item-content"
                 }
               >
-                {item.value}
-                <button>Button</button>
+                <div className="accordion-value">{item.value}</div>
+                <button className={item.buttonType}>
+                  {" "}
+                  <i className={item.buttonIcon}></i> {item.buttonName}
+                </button>
               </div>
             </div>
           ))}
