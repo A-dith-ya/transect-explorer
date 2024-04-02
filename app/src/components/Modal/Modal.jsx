d573c23bff0c22994f16b6b0134d028b5f7ed765
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-04-02 05:28:57 +0000

    Delete Transect Frontend

--- app/src/components/Modal/Modal.jsx
+++ app/src/components/Modal/Modal.jsx
@@ -4,7 +4,13 @@ import FormContainer from "../rjsf/FormContainer";
 import UISchemas from "../rjsf/UISchema/UISchema";
 import { loginFormSchema } from "../rjsf/schema/LoginFormSchema";
 
-export default function Modal({ modal, setModal, formSchema, uiSchema }) {
+export default function Modal({
+  modal,
+  setModal,
+  formSchema,
+  uiSchema,
+  handleSubmit,
+}) {
   const toggleModal = () => {
     setModal(!modal);
   };
@@ -21,7 +27,11 @@ export default function Modal({ modal, setModal, formSchema, uiSchema }) {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <FormContainer schema={formSchema} uiSchema={uiSchema} />
+            <FormContainer
+              schema={formSchema}
+              uiSchema={uiSchema}
+              onSubmitAction={handleSubmit}
+            />
             <button className="close-modal" onClick={toggleModal}>
               <i className="fa-solid fa-xmark"></i>
             </button>
